d99a5583f4fc2e811c6eec3027d87f8d70412306
utsav-devadiga <utsavdevadiga10@gmail.com>
2022-11-16 22:01:00 +0530

    Added loading Layout and gradle update

--- app/src/main/java/com/project/moviebag/utils/Credentials.java
+++ app/src/main/java/com/project/moviebag/utils/Credentials.java
@@ -6,5 +6,5 @@ public class Credentials {
 
     public static final String BASE_URL = "https://api.themoviedb.org/";
 
-    public static final String API_KEY = "10ee13e8ff32a27f7230b3dd8d801b96";
+    public static final String API_KEY = "";
 }
